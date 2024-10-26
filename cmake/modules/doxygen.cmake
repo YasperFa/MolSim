6657cae905f4e0de5ba91667d61342669e0d64fe
Stefanie Blattenberger <ge84dey@mytum.de>
2024-10-26 17:10:06 +0200

    custom target to create doxygen documentation with option to turn off

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -3,3 +3,23 @@
 # TODO: create CMake build option for the target.
 
 # TODO: Add a custom target for building the documentation.
+
+# create make target
+
+find_package(Doxygen) #sets the variable DOXYGEN_FOUND
+
+option(DOXY "enable doxygen" true)  #This only applies if DOXY has not been set (via command line or in the cache)
+
+if (DOXY AND DOXYGEN_FOUND) 
+add_custom_target(doc_doxygen "" EXCLUDE_FROM_ALL 
+                    COMMAND doxygen
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    COMMENT 
+                    "generating doxygen" 
+                    VERBATIM)
+endif()
+
+
+
+
+
