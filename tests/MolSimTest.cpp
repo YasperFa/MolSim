1ab7f5f400a9d2623aad99d01ea0b3a559699f22
Stefanie Blattenberger <ge84dey@mytum.de>
2024-11-11 01:01:28 +0100

    integrate spdlog, more tests for ParticleContainer and Calculator

--- tests/MolSimTest.cpp
+++ tests/MolSimTest.cpp
@@ -2,7 +2,11 @@
 // Created by Yasmine Farah on 01/11/2024.
 //
 #include "gtest/gtest.h"
+#include "spdlog/spdlog.h"
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    spdlog::set_level(spdlog::level::trace);
+
     return RUN_ALL_TESTS();
 }
--- tests/ParticleContainerTest.cpp
+++ tests/ParticleContainerTest.cpp
@@ -5,11 +5,42 @@
 #include "../src/Objects/ParticleContainer.h"
 #include "../src/Objects/Particle.h"
 
-// check if sizeParticles() works correctly
+/* Checks if sizeParticles() and addParticle() works correctly */
 TEST(ParticleContainerTest, StrctureAfterAddParticle) {
     ParticleContainer test;
     Particle p(0);
+    EXPECT_EQ(test.sizeParticles(), 0);
     test.addParticle(p);
     EXPECT_EQ(test.sizeParticles(), 1);
+    test.removeParticle(p);
+    EXPECT_EQ(test.sizeParticles(), 0);
+    
+
+}
+
+/* Checks if pairs are initialized correctly*/
+TEST(ParticleContainerTest, PairsAfterAddParticle) {
+    ParticleContainer test;
+    Particle p(0);
+    Particle q(1);
+    test.addParticle(p);
+    test.addParticle(q);
+
+    test.initializePairsVector();
+    EXPECT_EQ(test.getParticlePairs().size(), 1);
+
+    Particle r(1);
+    test.addParticle(r);
+    test.reinitializePairs();
+    EXPECT_EQ(test.getParticlePairs().size(), 3);
+
+    Particle s(1);
+    test.addParticle(s);
+    test.reinitializePairs();
+    EXPECT_EQ(test.getParticlePairs().size(), 6);
+
+    test.removeParticle(s);
+    test.reinitializePairs();
+    EXPECT_EQ(test.getParticlePairs().size(), 3);
 
 }
