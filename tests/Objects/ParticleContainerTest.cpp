3faf99e3611cc3636b71bbb6ed46d9065e91867a
Stefanie Blattenberger <ge84dey@mytum.de>
2024-11-28 02:14:34 +0100

    added test addParticle, added test for id initializer

--- tests/Objects/ParticleContainerTest.cpp
+++ tests/Objects/ParticleContainerTest.cpp
@@ -5,69 +5,82 @@
 #include "../../src/Objects/Containers/DirectSum/DirectSumContainer.h"
 #include "../src/Objects/Particle.h"
 
-/* Checks if sizeParticles() and addParticle() works correctly */
+/** Checks if sizeParticles() and addParticle() works correctly */
 TEST(ParticleContainerTest, StrctureAfterAddParticle) {
-    DirectSumContainer test;
+    DirectSumContainer testContainer;
     Particle p(0);
     
-    EXPECT_EQ(test.sizeParticles(), 0);
-    test.addParticle(p);
-    EXPECT_EQ(test.sizeParticles(), 1);
+    EXPECT_EQ(testContainer.sizeParticles(), 0);
+    testContainer.addParticle(p);
+    EXPECT_EQ(testContainer.sizeParticles(), 1);
 
-    test.removeParticle(p);
-    EXPECT_EQ(test.sizeParticles(), 0);
+    testContainer.removeParticle(p);
+    EXPECT_EQ(testContainer.sizeParticles(), 0);
     
 
 }
 
 
-/* Checks if addParticleToPairs() and removeParticle() work correctly*/
-/*TEST(ParticleContainerTest, addParticleToPairs) {
-    ParticleContainer test;
+/** Checks if addParticle() and removeParticle() work correctly*/
+TEST(ParticleContainerTest, addParticle) {
+    DirectSumContainer testContainer;
     
     Particle p(0);
-    test.addParticleToPairs(p);
-    EXPECT_EQ(test.getParticlePairs().size(), 0);
-    EXPECT_EQ(test.getParticles().size(), 1);
-    EXPECT_NO_THROW(test.getParticle(p));
+    testContainer.addParticle(p);
+    
+    EXPECT_EQ(testContainer.getParticles().size(), 1);
+    EXPECT_NO_THROW(testContainer.getParticle(p));
 
     Particle q(0);
-    test.addParticleToPairs(q);
-    EXPECT_EQ(test.getParticlePairs().size(), 1);
-    EXPECT_EQ(test.getParticles().size(), 2);
-    EXPECT_NO_THROW(test.getParticle(q));
+    testContainer.addParticle(q);
+    EXPECT_EQ(testContainer.getParticles().size(), 2);
+    EXPECT_NO_THROW(testContainer.getParticle(q));
 
     Particle r(0);
-    test.addParticleToPairs(r);
-
-    EXPECT_EQ(test.getParticlePairs().size(), 3);
-    EXPECT_EQ(test.getParticles().size(), 3);
-    EXPECT_NO_THROW(test.getParticle(r));
+    testContainer.addParticle(r);
+    EXPECT_EQ(testContainer.getParticles().size(), 3);
+    EXPECT_NO_THROW(testContainer.getParticle(r));
 
     Particle s(0);
-    test.addParticleToPairs(s);
-    EXPECT_EQ(test.getParticlePairs().size(), 6);
-    EXPECT_EQ(test.getParticles().size(), 4);
-    EXPECT_NO_THROW(test.getParticle(s));
+    testContainer.addParticle(s);
+    EXPECT_EQ(testContainer.getParticles().size(), 4);
+    EXPECT_NO_THROW(testContainer.getParticle(s));
 
-    test.removeParticle(r);
-    EXPECT_EQ(test.getParticlePairs().size(), 3);
-    EXPECT_EQ(test.getParticles().size(), 3);
-    EXPECT_ANY_THROW(test.getParticle(r));
+    testContainer.removeParticle(r);
+    EXPECT_EQ(testContainer.getParticles().size(), 3);
+    EXPECT_ANY_THROW(testContainer.getParticle(r));
 
-    test.removeParticle(p);
-    EXPECT_EQ(test.getParticlePairs().size(), 1);
-    EXPECT_EQ(test.getParticles().size(), 2);
-    EXPECT_ANY_THROW(test.getParticle(p));
+    testContainer.removeParticle(p);
+    EXPECT_EQ(testContainer.getParticles().size(), 2);
+    EXPECT_ANY_THROW(testContainer.getParticle(p));
 
-    test.removeParticle(s);
-    EXPECT_EQ(test.getParticlePairs().size(), 0);
-    EXPECT_EQ(test.getParticles().size(), 1);
-    EXPECT_ANY_THROW(test.getParticle(s));
+    testContainer.removeParticle(s);
+    
+    EXPECT_EQ(testContainer.getParticles().size(), 1);
+    EXPECT_ANY_THROW(testContainer.getParticle(s));
 
-    test.removeParticle(q);
-    EXPECT_EQ(test.getParticlePairs().size(), 0);
-    EXPECT_EQ(test.getParticles().size(), 0);
-    EXPECT_ANY_THROW(test.getParticle(q));
+    testContainer.removeParticle(q);
+    
+    EXPECT_EQ(testContainer.getParticles().size(), 0);
+    EXPECT_ANY_THROW(testContainer.getParticle(q));
 
-}*/
+}
+
+/** Tests that ids of new particles get initialized correctly */
+TEST(ParticleIdInitializerTest, testIDsAreUnique) {
+    ParticleIdInitializer::reset();
+    Particle p(1);
+    EXPECT_EQ(p.getID(), 1);
+
+    Particle q(1);
+    EXPECT_EQ(q.getID(), 2);
+
+    Particle r = Particle(p); //creating a particle from copy should copy the id
+    EXPECT_EQ(r.getID(), 1);
+
+    Particle s(1); //creating particles from copy should not influence new ids
+    EXPECT_EQ(s.getID(), 3);
+
+    s.makeShadowParticle();
+    EXPECT_EQ(s.getID(), -3);
+}
